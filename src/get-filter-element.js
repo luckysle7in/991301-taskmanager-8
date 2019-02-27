5a8cb914ecbde2ffbb11ac888f97129d0109bfa6
luckysle7in <luckysle7in@ya.ru>
2019-02-27 21:08:00 +0100

    Generate list of tasks based on object

--- src/get-filter-element.js
+++ src/get-filter-element.js
@@ -1,4 +1,21 @@
-export default (caption, amount, isChecked = false) => {
+// Get random number from 0 to MAX
+const getRandomNumber = (max) => {
+  return Math.floor(Math.random() * max);
+};
+
+// Object with content for filters
+const filters = [
+  {name: `All`, numberOfIssues: getRandomNumber(10), isChecked: true},
+  {name: `Overdue`, numberOfIssues: getRandomNumber(10), isChecked: false},
+  {name: `Today`, numberOfIssues: getRandomNumber(10), isChecked: false},
+  {name: `Favorites`, numberOfIssues: getRandomNumber(10), isChecked: false},
+  {name: `Repeating`, numberOfIssues: getRandomNumber(10), isChecked: false},
+  {name: `Tags`, numberOfIssues: getRandomNumber(10), isChecked: false},
+  {name: `Archive`, numberOfIssues: getRandomNumber(10), isChecked: false}
+];
+
+// Get code for one filter
+const getFilterElement = (caption, amount, isChecked = false) => {
   const captionLowerCase = caption.toLowerCase();
   return `
     <input
@@ -9,8 +26,28 @@ export default (caption, amount, isChecked = false) => {
       ${isChecked ? `checked` : ``}
       ${amount === 0 ? `disabled` : ``}
     />
-    <label for="filter__${captionLowerCase}" class="filter__label">
-      ${caption} <span class="filter__${captionLowerCase}-count">${amount}</span></label
+    <label
+      for="filter__${captionLowerCase}"
+      class="filter__label"
+      data-filter="${captionLowerCase}"
+      data-filter-count="${amount}"
     >
+      ${caption} <span class="filter__${captionLowerCase}-count">${amount}</span>
+    </label>
   `;
 };
+
+// Get code for the list of the filters
+export default () => {
+  let filtersCode = ``;
+  filters.forEach((filter) => {
+    filtersCode += getFilterElement(
+        filter[`name`],
+        filter[`numberOfIssues`],
+        filter[`isChecked`]
+    );
+  });
+  return filtersCode;
+};
+
+export {filters};
